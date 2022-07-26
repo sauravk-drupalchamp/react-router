d0fc0bad26efe7cbc2a10d22d125131f4f46bb77
Saurav Kaushik <sauravk.drupalchamp@gmail.com>
2022-07-26 13:49:42 +0530

    Links

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import './App.css';
-import {Homepage, About} from './components';
+import "./App.css";
+import { Homepage, About, Navbar } from "./components";
 import { Route, Routes } from "react-router-dom";
+import { Fragment } from "react";
 
 function App() {
   return (
+    <Fragment>
+      <Navbar />
       <Routes>
-        <Route path='/' exact element={<Homepage />}/>
-        <Route path='/about' exact element={<About />}/>
+        <Route path="/" exact element={<Homepage />} />
+        <Route path="/about" exact element={<About />} />
       </Routes>
+    </Fragment>
   );
 }
 
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export  {default as Homepage } from '../components/Homepage';
 export  {default as About } from '../components/About';
+export  {default as Navbar } from '../components/Navbar';
