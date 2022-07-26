2fa6719d7fa96aba11af8b5be20a191857b36bcd
Saurav Kaushik <sauravk.drupalchamp@gmail.com>
2022-07-26 14:40:01 +0530

    Active Link Styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,18 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 const Navbar = () => {
+    // ACTIVE LINK STYLE USING FUNCTION
+    const activeStyle = ({ isActive }) => {
+        return {
+            fontWeight: isActive ? 'bold' : 'normal',
+            textDecoration: isActive ? 'none' : 'underline'
+        }
+    }
+
   return (
     <nav>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
+      <NavLink style={activeStyle} to="/">Home</NavLink>
+      <NavLink style={activeStyle} to="/about">About</NavLink>
     </nav>
   );
 };
